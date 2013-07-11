62b8abc3d9e5e1419cda5c65b3669b402b0014d1
Borovilov Ruslan <pycz2007@yandex.ru>
2013-07-11 18:36:41 +0400

    Empty Map done

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ Created on 11.07.2013
 @author: pycz
 '''
 
+class State:
+    empty = 0
+    kill = 1
+    miss = 2
+
+class Cell:
+    def __init__(self, state):
+        self.state = state
+        
+
+class Map:
+    def __init__(self):
+        self.make_empty()
+        
+    def fill_with_ships(self):
+        pass
+        
+    def make_empty(self):
+        m = [[Cell(State.empty) for j in xrange(10)] for i in xrange(10)]
+        self.map = m
+
+class Bot:
+    pass
+
+
 if __name__ == '__main__':
     pass
