2f2c67585acacaa7e45eea368b3680ad75c61f4d
Borovilov Ruslan <pycz2007@yandex.ru>
2013-07-19 00:07:44 +0400

    Add shipfilling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created on 11.07.2013
 @author: pycz
 '''
 import sys
-from random import randint
+import random
 
 def to_num_coord(strCoord):    # return in tuple (letter, number)
     part1 = ord(strCoord[0]) - ord("a")
@@ -31,8 +31,95 @@ class Map:
         self.make_empty()
         
     def fill_with_ships(self):
-        pass
-        
+        self.make_empty()
+        grid = self.map
+        self.make_empty()
+        ship_len = 4
+        while ship_len > 0:
+            ship_count = 5 - ship_len  #how many ships of a kind are?
+            for m in xrange(ship_count):
+                possibilities = []
+                # check horizontally
+                b_i = 0
+                b_j = 0
+                e_i = ship_len-1
+                e_j = 0
+                while (e_i <= 9) and (e_j <= 9):
+                    good = True
+                    i = b_i
+                    j = b_j
+                    for n in xrange(ship_len):
+                        good = good and (grid[i+n][j].state == State.empty)
+                        if not good:
+                            break
+                    if good:
+                        possibilities.append( ((b_i, b_j), (e_i, e_j)) )
+                    
+                    if e_i < 9:
+                        b_i += 1
+                        e_i += 1
+                    else:
+                        b_i = 0
+                        b_j +=1
+                        e_i = ship_len-1
+                        e_j +=1          
+
+                # check vertically
+                b_i = 0
+                b_j = 0
+                e_i = 0
+                e_j = ship_len-1
+                while (e_i <= 9) and (e_j <= 9):
+                    good = True
+                    i = b_i
+                    j = b_j
+                    for n in xrange(ship_len):
+                        good = good and (grid[i][j+n].state == State.empty)
+                        if not good:
+                            break
+                    if good:
+                        possibilities.append( ((b_i, b_j), (e_i, e_j)) )
+                    
+                    if e_j < 9:
+                        b_j += 1
+                        e_j += 1
+                    else:
+                        b_j = 0
+                        b_i +=1
+                        e_j = ship_len-1
+                        e_i +=1                 
+                
+                # and pick a random position and place it
+                pos = random.choice(possibilities)
+                b_i = pos[0][0]
+                b_j = pos[0][1]
+                e_i = pos[1][0]
+                e_j = pos[1][1] 
+                while b_i <= e_i:
+                    b_j = pos[0][1]
+                    while b_j <= e_j:
+                        self.map[b_i][b_j].state = State.ship
+                        grid[b_i][b_j].state = State.ship
+                        # where cant place
+                        n_i = b_i - 1
+                        while n_i <= b_i + 1:
+                            n_j = b_j - 1
+                            while n_j <= b_j + 1:
+                                if (not(b_i == n_i and b_j == n_j) and 
+                                        0 <= n_j <= 9 and
+                                        0 <= n_i <= 9 and
+                                        grid[n_i][n_j].state == State.empty):
+                                    grid[n_i][n_j].state = State.miss
+                                n_j += 1
+                            n_i += 1
+                        b_j += 1
+                    b_i +=1
+                # next ship done!
+                #self.show_map()
+            ship_len -= 1
+            
+            
+            
     def make_empty(self):
         self.map = [[Cell(State.empty) for j in xrange(10)] for i in xrange(10)]
         
@@ -66,4 +153,6 @@ class Bot:
 
 
 if __name__ == '__main__':
-    Map().show_map()
+    m = Map()
+    m.fill_with_ships()
+    m.show_map()
