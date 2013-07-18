579a6d39017b1077671afd3e8c47735cf9c8132e
Borovilov Ruslan <pycz2007@yandex.ru>
2013-07-18 21:31:34 +0400

    Add show_map method

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ Created on 11.07.2013
 
 @author: pycz
 '''
-
+import sys
 from random import randint
 
-def to_num_coord(strCoord):
+def to_num_coord(strCoord):    # return in tuple (letter, number)
     part1 = ord(strCoord[0]) - ord("a")
     if len(strCoord) == 2:
         part2 = int(strCoord[1]) - 1
     else:
         part2 = 9
-    return part1 + part2
+    return (part1, part2)
         
 
 class State:
     empty = 0
     kill = 1
     miss = 2
+    ship = 3
 
 class Cell:
     def __init__(self, state):
@@ -33,12 +34,36 @@ class Map:
         pass
         
     def make_empty(self):
-        m = [[Cell(State.empty) for j in xrange(10)] for i in xrange(10)]
-        self.map = m
+        self.map = [[Cell(State.empty) for j in xrange(10)] for i in xrange(10)]
+        
+    def show_map(self):
+        sys.stdout.write("  ")
+        for i in xrange(10):
+            sys.stdout.write(chr(ord('a')+i))
+        sys.stdout.write('\n')
+        for i in xrange(10):
+            if i<9:
+                sys.stdout.write(" ")
+            sys.stdout.write(str(i+1))
+            for j in xrange(10):
+                state_now = self.map[j][i].state
+                if state_now == State.empty:
+                    cage = " "
+                elif state_now == State.kill:
+                    cage = "X"
+                elif state_now == State.miss:
+                    cage = "O"
+                else:
+                    cage = "#"
+                
+                sys.stdout.write(str(cage))
+            sys.stdout.write('\n')
+        
+        
 
 class Bot:
     pass
 
 
 if __name__ == '__main__':
-    pass
+    Map().show_map()
